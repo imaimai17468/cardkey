fda3308f55989d57d3ecaa45db0cd44e9d4484fe
Hasegawa2718 <tosiki_801_life@outlook.jp>
2021-07-18 16:13:18 +0900

    RTC周りを改良

--- cardkey.ino
+++ cardkey.ino
@@ -1,17 +1,23 @@
-#include <RTClib.h>
 #include <LiquidCrystal_I2C.h>
 #include <SD.h>
 #include <RCS620S.h>
 #include <avr/pgmspace.h>
 #include <Servo.h>
+#include<SPI.h>
+#include <Wire.h>
+#include <DS3231.h>
 
 #define LCD_I2C 0x27
 
 const char IDm001[] PROGMEM = "01120412D417FF0A";
-const char* const IDms[] PROGMEM = { IDm001 };
+const char IDm002[] PROGMEM = "01100310F70F9D12";
+const char IDm003[] PROGMEM = "0139847C4787E6F5";
+const char* const IDms[] PROGMEM = { IDm001, IDm002, IDm003 };
 
-const char name001[] PROGMEM = "imaimai";
-const char* const names[] PROGMEM = { name001 };
+const char name001[] PROGMEM = "Toshiki Imai";
+const char name002[] PROGMEM = "Ryousuke Yagi";
+const char name003[] PROGMEM = "Yousuke Yoshizawa";
+const char* const names[] PROGMEM = { name001, name002, name003 };
 
 bool *is_enter;
 int num_felica;
@@ -23,21 +29,10 @@ int num_felica;
 
 Servo myservo;
 LiquidCrystal_I2C lcd(LCD_I2C, 20, 4);
-RTC_DS3231 rtc;
+DS3231 clock;
+RTCDateTime dt;
 RCS620S rcs620s;
-
-// 日付の文字列から年などを取得
-int convToNum(char* buf, int start, int digit){
-  int i, num = 0, pow = 1, pos;
-
-  pos = start + digit - 1;
-  for (i = 0; i < digit; i++) {
-    num += (buf[pos] - '0') * pow;
-    pow *= 10;
-    pos--;
-  }
-  return num;
-}
+File memory_file, status_file;
 
 void setup() {
   int y = 0, ret = 0, i;
@@ -46,6 +41,7 @@ void setup() {
   
   Serial.begin(115200);
   Serial1.begin(115200);
+  Serial.println("Serial start");
 
   // 変数の初期化
   num_felica = sizeof(names) / sizeof(char*);
@@ -55,52 +51,40 @@ void setup() {
   myservo.write(90);
   delay(500);
   myservo.detach();
+  Serial.println("servo init ok");
+
+  // RTCの初期化
+  clock.begin();
+  clock.setDateTime(__DATE__, __TIME__);
+  Serial.println("Initialize DS3231");
   
   // LCDの初期化
   lcd.init();
+  Serial.println("LCD init ok");
+  
   lcd.backlight();
   lcd.clear();
   
   // SDの初期化
   lcd.setCursor(0, y);
+  pinMode(SD_CS, OUTPUT);
   if (!SD.begin(SD_CS)) {
     lcd.print("SD failed!");
+    Serial.println("SD failed!");
     while (1);
   }
   lcd.print("SD init OK");
+  Serial.println("SD init OK");
   y++;
 
   // 入退室情報の初期化
   if(SD.exists("enter.txt")){
-    File f = SD.open("enter.txt");
-    f.read(buf, num_felica*2);
-    f.close();
+    status_file = SD.open("enter.txt");
+    status_file.read(buf, num_felica*2);
+    status_file.close();
     for(i = 0; i < num_felica*2; i += 2){
       is_enter[i/2] = buf[i] - '0';
     }
-  }
-
-  // 日時の初期化
-  if(SD.exists("time.txt")){
-    File f = SD.open("time.txt");
-    f.read(buf, 29);
-    f.close();
-    ye = convToNum(buf, 0, 4);
-    mo = convToNum(buf, 5, 2);
-    da = convToNum(buf, 8, 2);
-    ho = convToNum(buf, 11, 2);
-    mi = convToNum(buf, 14, 2);
-    se = convToNum(buf, 17, 2);
-    //setTime(ho, mi, se, da, mo, ye);
-    rtc.adjust(DateTime(ye, mo, da, ho, mi, se));
-    SD.remove("time.txt");
-    lcd.setCursor(0, y);
-    lcd.print("Set time");
-    y++;
-    lcd.setCursor(0, y);
-    sprintf(buf, "%04d/%02d/%02d %02d:%02d:%02d", ye, mo, da, ho, mi, se);
-    lcd.print(buf);
-    y++;
   }
   
   // RC-S620Sの初期化
@@ -109,6 +93,7 @@ void setup() {
   rcs620s.timeout = COMMAND_TIMEOUT;
   lcd.setCursor(0, y);
   lcd.print("RC-S620S init ok");
+  Serial.println("RC-S620S init ok");
   y++;
 
   // 初期表示
@@ -122,15 +107,15 @@ void loop() {
   int ret, i;
   String IDm = "";
   char buf[30], fname[15], uname[30], IDm_buf[17], date[30];
-  DateTime t;
+  RTCDateTime t;
 
   // サーボモータを動かさない間は開放する
   myservo.detach();
 
   // 時刻の表示
-  t = rtc.now();
+  t = clock.getDateTime();
   lcd.setCursor(0, 0);
-  sprintf(date, "%04d/%02d/%02d %02d:%02d:%02d", t.year(), t.month(), t.day(), t.hour(), t.minute(), t.second());
+  sprintf(date, "%04d/%02d/%02d %02d:%02d:%02d", t.year, t.month, t.day, t.hour, t.minute, t.second);
   lcd.print(date);
   lcd.setCursor(0, 1);
   lcd.print("Wait FeliCa ");
@@ -142,8 +127,9 @@ void loop() {
   
   // FeliCaがタッチされた場合
   if(ret){
-    sprintf(fname, "%04d%02d%02d", t.year(), t.month(), t.day());
+    sprintf(fname, "%04d%02d%02d", t.year, t.month, t.day);
     strcat(fname, ".csv");
+    Serial.println(fname);
     
     // IDmを取得する
     for(i = 0; i < 8; i++){
@@ -192,46 +178,46 @@ void loop() {
     lcd.setCursor(0, 2);
     lcd.print(uname);
 
-    // 入退室記録を書き込み
-    File f = SD.open(fname, FILE_WRITE);
-    if (f){
-      for (i = 0; i < num_felica; i++) {
-        strcpy_P(IDm_buf, (char*) pgm_read_word(&IDms[i]));
-        strcpy_P(uname, (char*) pgm_read_word(&names[i]));
-        f.print(IDm_buf);
-        f.print(",");
-        f.print(uname);
-        f.print(",");
-        f.print(date);
-        f.print(",");
-        f.println((is_enter[i] ? "Enter" : "Leave"));
+    Serial.print(IDm_buf);
+    Serial.print(",");
+    Serial.print(uname);
+    Serial.print(",");
+    Serial.print(date);
+    Serial.print(",");
+    Serial.println((is_enter[i] ? "Enter" : "Leave"));
 
-        Serial.print(IDm_buf);
-        Serial.print(",");
-        Serial.print(uname);
-        Serial.print(",");
-        Serial.print(date);
-        Serial.print(",");
-        Serial.println((is_enter[i] ? "Enter" : "Leave"));
-      }
-      f.close();
+        
+    // 入退室記録を書き込み
+    memory_file = SD.open(fname, FILE_WRITE);
+    if (memory_file){
+      memory_file.print(IDm_buf);
+      memory_file.print(",");
+      memory_file.print(uname);
+      memory_file.print(",");
+      memory_file.print(date);
+      memory_file.print(",");
+      memory_file.println((is_enter[i] ? "Enter" : "Leave"));
+      memory_file.close();
     }else{
-      Serial.println("File open error");
+      Serial.println("File open error [memory]");
+      memory_file.close();
     }
 
     // 入退室の状態を書き込み
     // 一度消して更新する
+    
     SD.remove("enter.txt");
-    f = SD.open("enter.txt", FILE_WRITE);
-    if (f){
+    status_file = SD.open("enter.txt", FILE_WRITE);
+    if (status_file){
       for (i = 0; i < num_felica; i++) {
-        f.print(is_enter[i]);
-        f.print(",");
+        status_file.print(is_enter[i]);
+        status_file.print(",");
       }
-      f.println();
-      f.close();
+      status_file.println();
+      status_file.close();
     }else{
-      Serial.println("File open error");
+      Serial.println("File open error [status]");
+      status_file.close();
     }
   }
 
